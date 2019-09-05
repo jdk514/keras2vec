bd1d5779a0ab70336a2edb892ed880558aaf5d2e
Joel Klein <jdk51405@gmail.com>
2019-09-05 00:16:10 -0400

    Added additional components to model, enabled flexible inputs, updated gitignore

--- keras2vec/keras2vec.py
+++ keras2vec/keras2vec.py
@@ -1,4 +1,6 @@
-from keras.layers import Input, Embedding, Average, Dot, Dense
+import tensorflow as tf
+
+from keras.layers import Input, Embedding, Average, Dot, Dense, Lambda, Concatenate
 from keras.models import Model
 
 from keras2vec.data_generator import DataGenerator
@@ -29,8 +31,20 @@ class Keras2Vec:
         """Build the keras model to embed documents"""
 
         doc_ids = Input(shape=(1,))
-        labels = Input(shape=(self.num_labels,))
         sequence = Input(shape=(self.seq_size,))
+        if self.num_labels > 0:
+            labels = Input(shape=(self.num_labels,))
+
+            label_embedding = Embedding(input_dim=self.label_vocab,
+                                        output_dim=self.embedding_size,
+                                        input_length=self.num_labels,
+                                        name="label_embedding")(labels)
+
+            if self.num_labels > 1:
+                split_label = Lambda(self.split_layer())(label_embedding)
+                avg_labels = Average()(split_label)
+            else:
+                avg_labels = label_embedding
 
         doc_inference = Embedding(input_dim=1,
                                   output_dim=self.embedding_size,
@@ -42,23 +56,32 @@ class Keras2Vec:
                                   input_length=1,
                                   name="doc_embedding")(doc_ids)
 
-        label_embedding = Embedding(input_dim=self.label_vocab,
-                                    output_dim=self.embedding_size,
-                                    input_length=self.num_labels,
-                                    name="label_embedding")(labels)
-
         seq_embedding = Embedding(input_dim=self.word_vocab,
                                   output_dim=self.embedding_size,
                                   input_length=self.seq_size,
                                   name="word_embedding")(sequence)
 
-        context = Average()([label_embedding, seq_embedding])
+        if self.seq_size > 1:
+            split_seq = Lambda(self.split_layer())(seq_embedding)
+            avg_seq = Average()(split_seq)
+        else:
+            avg_seq = seq_embedding
+
+        if self.num_labels > 0:
+            context = Average()([avg_labels, avg_seq])
+        else:
+            context = avg_seq
 
         # Build training model
-        train_merged = Dot()(doc_embedding, context)
+        train_merged = Average()([doc_embedding, context])
         train_output = Dense(1, activation='sigmoid')(train_merged)
-        train_model = Model(inputs=[doc_embedding, label_embedding, seq_embedding],
-                            outputs=train_output)
+
+        if self.num_labels > 0:
+            train_model = Model(inputs=[doc_ids, labels, sequence],
+                                outputs=train_output)
+        else:
+            train_model = Model(inputs=[doc_ids, sequence],
+                                outputs=train_output)
 
         train_model.compile(optimizer='adamax',
                             loss='binary_crossentropy',
@@ -67,15 +90,26 @@ class Keras2Vec:
 
 
         # Build model for inference
-        infer_merged = Dot()(doc_inference, context)
+        infer_merged = Average()([doc_inference, context])
         infer_output = Dense(1, activation='sigmoid')(infer_merged)
-        infer_model = Model(inputs=[doc_inference, label_embedding, seq_embedding],
+
+        if self.num_labels > 0:
+            infer_model = Model(inputs=[doc_ids, labels, sequence],
                             outputs=infer_output)
+        else:
+            infer_model = Model(inputs=[doc_ids, sequence], outputs=infer_output)
 
         infer_model.compile(optimizer='adamax',
                             loss='binary_crossentropy',
                             metrics=['accuracy'])
         self.train_model = infer_model
+
+
+    def split_layer(self):
+        def split_func(layer):
+            return tf.split(layer, self.embedding_size, axis=0)
+
+        return split_func
 
 
     def infer_vector(self):
