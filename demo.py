f122e61a4b0c8cc3de2c16b952deaf7f095f5ddd
Joel Klein <jdk51405@gmail.com>
2019-09-12 23:02:48 -0400

    Updated documentation for release of version 0.0.2

--- demo.py
+++ demo.py
@@ -3,37 +3,42 @@ from keras2vec.document import Document
 
 from sklearn.metrics.pairwise import euclidean_distances, cosine_similarity
 
-def layer_dist(embeddings, id_1, id_2):
-    doc1 = embeddings[id_1 - 1].reshape(1, -1)
-    doc2 = embeddings[id_2 - 1].reshape(1, -1)
-    return cosine_similarity(doc1, doc2)
+def doc_similarity(embeddings, id_1, id_2):
+    doc1 = embeddings[id_1].reshape(1, -1)
+    doc2 = embeddings[id_2].reshape(1, -1)
+    return cosine_similarity(doc1, doc2)[0][0] # , euclidean_distances(doc1, doc2)
 
 
 if __name__ == "__main__":
 
-    docs = [Document(1, [], "Test Document 01"),
-            Document(1, [], "Test Document 02"),
-            Document(1, [], "Test Document 03"),
-            Document(1, [], "Test Document 04"),
-            Document(2, [], "Test Document 05"),
-            Document(2, [], "Test Document 06"),
-            Document(2, [], "Test Document 07"),
-            Document(2, [], "Test Document 08"),
-            Document(3, [], "Random words to experiment"),
-            Document(3, [], "Random words to experiment"),
-            Document(3, [], "Random words to experiment"),
-            Document(3, [], "Random words to experiment"),
-            Document(4, [], "Random text to experiment"),
-            Document(4, [], "Random text to experiment"),
-            Document(4, [], "Random text to experiment"),
-            Document(4, [], "Random text to experiment")
-            ]
+    docs =["red yellow green blue orange violet green blue orange violet",
+           "blue orange green gray black teal tan blue violet gray black teal",
+           "blue violet gray black teal yellow orange tan white brown",
+           "black blue yellow orange tan white brown white green teal pink blue",
+           "orange pink blue white yellow black black teal tan",
+           "white green teal gray black pink blue blue violet gray black teal yellow",
+           "cat dog rat gerbil hamster goat lamb goat cow rat dog pig",
+           "lamb goat cow rat dog pig dog chicken goat cat cow pig",
+           "pig lamb goat rat gerbil dog cat dog rat gerbil hamster goat",
+           "dog chicken goat cat cow pig gerbil goat cow pig gerbil lamb",
+           "rat hamster pig dog chicken cat lamb goat cow rat dog pig dog",
+           "gerbil goat cow pig gerbil lamb rat hamster pig dog chicken cat"
+           ]
 
+    keras_docs = [Document(ix, [], doc) for ix, doc in enumerate(docs)]
 
-    doc2vec = Keras2Vec(docs, embedding_size=16, seq_size=1)
+    # TODO: Add ability to auto-select embedding and seq_size based on data
+    doc2vec = Keras2Vec(keras_docs, embedding_size=24, seq_size=1)
     doc2vec.build_model()
-    doc2vec.fit(15)
+    # If the number of epochs is to low, the check at the bottom may fail!
+    doc2vec.fit(25)
 
-    # TODO: Add method to pull doc/word/label embeddings
-    embeddings = doc2vec.train_model.get_layer('doc_embedding').get_weights()[0]
-    print(layer_dist(embeddings, 1, 4))
+    embeddings = doc2vec.get_doc_embeddings()
+
+    """Docs 0-5 are colors while 6-11 are animals. The cosine distances for
+    docs from the same topic (colors/animals) should approach 1, while
+    disimilar docs, coming from different topics, should approach -1"""
+    if doc_similarity(embeddings, 2, 4) > doc_similarity(embeddings, 2, 10):
+        print("Like topics are more similar")
+    else:
+        print("Something went wrong during training!")
--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = '2019, Joel Klein'
 author = 'Joel Klein'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.1'
+release = '0.0.2'
 
 
 # -- General configuration ---------------------------------------------------
