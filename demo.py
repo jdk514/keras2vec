738da0fbc44c2d43c9831d818fa5d3b9b494f096
Joel <jdk51405@gmail.com>
2019-09-26 22:17:02 -0400

    Alterations for testing label embeddings

--- demo.py
+++ demo.py
@@ -13,32 +13,40 @@ def doc_similarity(embeddings, id_1, id_2):
 
 if __name__ == "__main__":
 
-    docs = ["red yellow green blue orange violet green blue orange violet",
-            "blue orange green gray black teal tan blue violet gray black teal",
-            "blue violet gray black teal yellow orange tan white brown",
-            "black blue yellow orange tan white brown white green teal pink blue",
-            "orange pink blue white yellow black black teal tan",
-            "white green teal gray black pink blue blue violet gray black teal yellow",
-            "cat dog rat gerbil hamster goat lamb goat cow rat dog pig",
-            "lamb goat cow rat dog pig dog chicken goat cat cow pig",
-            "pig lamb goat rat gerbil dog cat dog rat gerbil hamster goat",
-            "dog chicken goat cat cow pig gerbil goat cow pig gerbil lamb",
-            "rat hamster pig dog chicken cat lamb goat cow rat dog pig dog",
-            "gerbil goat cow pig gerbil lamb rat hamster pig dog chicken cat"
-           ]
+    color_docs = ["red yellow green blue orange violet green blue orange violet",
+                   "blue orange green gray black teal tan blue violet gray black teal",
+                   "blue violet gray black teal yellow orange tan white brown",
+                   "black blue yellow orange tan white brown white green teal pink blue",
+                   "orange pink blue white yellow black black teal tan",
+                   "white green teal gray black pink blue blue violet gray black teal yellow",
+                   ]
+
+    animal_docs = ["cat dog rat gerbil hamster goat lamb goat cow rat dog pig",
+                   "lamb goat cow rat dog pig dog chicken goat cat cow pig",
+                   "pig lamb goat rat gerbil dog cat dog rat gerbil hamster goat",
+                   "dog chicken goat cat cow pig gerbil goat cow pig gerbil lamb",
+                   "rat hamster pig dog chicken cat lamb goat cow rat dog pig dog",
+                   "gerbil goat cow pig gerbil lamb rat hamster pig dog chicken cat"
+                   ]
+
+    animal_color_docs = ['goat green rat gerbil yellow dog cat blue white',
+                         'gerbil black pink blue lamb rat hamster gray pig dog',
+                         'orange pink cat cow pig black teal gerbil tan',
+                         'hamster pig orange violet dog chicken orange tan']
 
     inference_doc = "red yellow green blue orange violet green blue orange violet"
 
+    doc_count = 0
     keras_docs = []
-    for ix, doc in enumerate(docs):
-        if ix < 6:
-            label = 'colors'
-        else:
-            label = 'animals'
-        keras_docs.append(Document(ix, [label], doc))
+
+    keras_docs.extend([Document(doc_count+ix, text, ['color']) for ix, text in enumerate(color_docs)])
+    doc_count = len(keras_docs)
+    keras_docs.extend([Document(doc_count+ix, text, ['animal']) for ix, text in enumerate(animal_docs)])
+    doc_count = len(keras_docs)
+    keras_docs.extend([Document(doc_count + ix, text, ['animal', 'color']) for ix, text in enumerate(animal_color_docs)])
 
     # TODO: Add ability to auto-select embedding and seq_size based on data
-    doc2vec = Keras2Vec(keras_docs, embedding_size=24, seq_size=1)
+    doc2vec = Keras2Vec(keras_docs, embedding_size=24, seq_size=3)
     doc2vec.build_model()
     # If the number of epochs is to low, the check at the bottom may fail!
     doc2vec.fit(25)
@@ -56,8 +64,8 @@ if __name__ == "__main__":
     """Using the trained model we can now infer document vectors by training
     against a model where only the inference layer is trainable"""
 
-    doc2vec.infer_vector(Document(0, [], inference_doc), lr=.1, epochs=5)
+    doc2vec.infer_vector(Document(0, inference_doc, []), lr=.1, epochs=5)
     infer_vec = doc2vec.get_infer_embedding()
-    infer_dist = cosine_similarity(infer_vec.reshape(1, -1), embeddings[0].reshape(1, -1))[0][0]
+    infer_dist = cosine_similarity(infer_vec.reshape(1, -1), embeddings[-1].reshape(1, -1))[0][0]
     infer_dist = "{0:0.2f}".format(infer_dist)
     print(f'Document 0 has a cosine similarity of {infer_dist} between train and inferred vectors')
